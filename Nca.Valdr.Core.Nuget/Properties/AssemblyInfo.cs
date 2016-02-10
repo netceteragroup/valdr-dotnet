a57506f3d8224a2341926379c271f5943eec0422
Alex Ullrich <alex.ullrich@gmail.com>
2016-02-10 11:55:25 -0500

    add nuget project for Nca.Valdr.Core

---
+++ Nca.Valdr.Core.Nuget/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nca.Valdr.Nuget")]
+[assembly: AssemblyDescription("NuGet package")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
